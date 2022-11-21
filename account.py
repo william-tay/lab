694f944fb3c66edd1d793c656b3d5716f9498f24
Will Taylor <106984134+william-tay@users.noreply.github.com>
2022-11-20 20:40:00 -0600

    Update account.py

--- account.py
+++ account.py
@@ -5,10 +5,10 @@ class Account:
         an initial balance of 0 for the account
         :param name: This parameter takes in the name for the account
         """
-        self.account_name = name
-        self.account_balance = 0
+        self.__account_name = name
+        self.__account_balance = 0
 
-    def deposit(self, amount: float) -> float:
+    def deposit(self, amount: float):
         """
         This is just a function that is used to deposit the money, it will take in the amount param
         and add that to the existing account balance
@@ -16,20 +16,20 @@ class Account:
         :return: returns the balance after depositing the intended amount
         """
         if float(amount) > 0:
-            self.account_balance = self.account_balance + amount
+            self.__account_balance = self.__account_balance + amount
             return True
         else:
             return False
 
-    def withdraw(self, amount: float) -> float:
+    def withdraw(self, amount: float):
         """
                This is just a function that is used to withdraw the money, it will take in the amount param
                and subtract that from the existing account balance
                :param amount: this is the amount which will be withdrawn into the account
                :return: returns the balance after withdrawing the intended amount
                """
-        if 0 < float(amount) < self.account_balance:
-            self.account_balance = self.account_balance - amount
+        if 0 < float(amount) <= self.__account_balance:
+            self.__account_balance -= amount
             return True
         else:
             return False
@@ -39,11 +39,11 @@ class Account:
         all this does is return the current balance
         :return: returns the current account balance
         """
-        return self.account_balance
+        return self.__account_balance
 
     def get_name(self) -> str:
         """
         all this does is return the current name
         :return: returns the current account name
         """
-        return self.account_name
+        return self.__account_name
